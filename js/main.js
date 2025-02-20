6f2d5d1aec3ab123c7052ce60535fb7ef39f5e48
srikk <122169588+py-code314@users.noreply.github.com>
2025-02-20 14:10:59 -0500

    feat: Add func to make whole card clickable

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+
+const cards = document.querySelectorAll('.card__content');
+console.log(cards);
+
+// Array.prototype.forEach.call() is a way to use 'forEach' method on non-array objects.
+Array.prototype.forEach.call(cards, (card) => {
+  // down & up variables are only initialized, link is assigned to link element.
+  let down,
+    up,
+    link = card.querySelector('.card__subtitle a');
+  card.style.cursor = 'pointer'
+
+  card.onmousedown = () => (down = +new Date()); // '+' converts Date object into a number.
+  card.onmouseup = () => {
+    up = +new Date();
+    if (up - down < 200) {
+      link.click();
+      console.log('link clicked');
+    }
+  };
+});
