3e63712c18a5fce5be0f5218d40bfe6b3d0c8d8a
srikk <122169588+py-code314@users.noreply.github.com>
2025-02-23 14:07:52 -0500

    feat: Add funcs updateAriaSelected, changeFocus

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const dailyBtn = document.querySelector('#daily');
 const weeklyBtn = document.querySelector('#weekly');
 const monthlyBtn = document.querySelector('#monthly');
-
+const cardWork = document.querySelector('.card--work');
 const cards = document.querySelectorAll('.card__content');
+const cardBtns = Array.from(document.querySelectorAll('.card__btn-duration'));
+// console.log(cardBtns);
 
 const cardNames = document.querySelectorAll('.card__subtitle a');
-
-
 
 /* Function to make whole card clickable & make text within copyable */
 // Array.prototype.forEach.call() is a way to use 'forEach' method on non-array objects.
@@ -15,76 +15,99 @@ Array.prototype.forEach.call(cards, (card) => {
   let down,
     up,
     link = card.querySelector('.card__subtitle a');
-  card.style.cursor = 'pointer'
+  card.style.cursor = 'pointer';
 
   card.onmousedown = () => (down = +new Date()); // '+' converts Date object into a number.
   card.onmouseup = () => {
     up = +new Date();
     if (up - down < 200) {
       link.click();
-      console.log('link clicked');
+      // console.log('link clicked');
     }
   };
 });
 
-async function fetchStats() {
+async function fetchTimeTrackingData() {
   try {
-    const response = await fetch("../data.json")
+    const response = await fetch('../data.json');
+
     if (!response.ok) {
-      throw new Error(`HTTP Error: ${response.status}`)
+      throw new Error(`HTTP Error: ${response.status}`);
     }
-    const data = await response.json()
-  
-    getDetails(data)
 
-   
+    const data = await response.json();
+    renderTimeTrackingData(data);
   } catch (error) {
-    console.error(`Could not get the data: ${error}`)
- } 
+    console.error(`Could not get the data: ${error}`);
+  }
 }
 
-fetchStats()
+// function renderTimeTrackingData(data) {
 
+fetchTimeTrackingData();
 
-function getDetails(data) {
- 
-  data.forEach(category => {
-    const { title, timeframes } = category;
-    
-    const { daily, weekly, monthly } = timeframes
-    
-    const titleId = title.split(' ').join('-').toLowerCase()
-    
+function renderTimeTrackingData(data) {
+  data.forEach(({ title, timeframes }) => {
+    const { daily, weekly, monthly } = timeframes;
+    const formattedTitleId = title.toLowerCase().replace(/\s+/g, '-');
 
-    // console.log('Daily: ', daily);
-    dailyBtn.addEventListener('click', () => showStats(title, titleId, daily))
+    cardBtns.forEach((btn) => {
+      btn.addEventListener('click', (event) => {
+        showStats(title, formattedTitleId, timeframes[btn.id]);
+        updateAriaSelected(event);
+        changeFocus(event)
+      });
+    });
 
-    // console.log('Weekly: ', weekly);
-    weeklyBtn.addEventListener('click', () => showStats(title, titleId, weekly))
-
-    // console.log('Monthly: ', monthly);
-    monthlyBtn.addEventListener('click', () => showStats(title, titleId, monthly))
-
-    showStats(title, titleId, daily)
-    
-
-  })
+    showStats(title, formattedTitleId, daily);
+  });
 }
 
-function showStats(title, titleId, timeframe) {
-  Array.from(cardNames).forEach((cardName) => {
-    if (cardName.textContent === title) {
-      const currentHrs = document.querySelector(`#current-hrs-${titleId}`);
-      currentHrs.textContent = timeframe.current;
-      const previousHrs = document.querySelector(`#previous-hrs-${titleId}`);
+function showStats(categoryTitle, categoryTitleId, { current, previous }) {
+  const cardTitleElements = document.querySelectorAll('.card__subtitle a');
 
-      previousHrs.textContent =
-        timeframe.previous === 1
-          ? `Last Week - ${timeframe.previous}hr`
-          : `Last Week - ${timeframe.previous}hrs`;
+  cardTitleElements.forEach((cardTitleElement) => {
+    if (cardTitleElement.textContent === categoryTitle) {
+      const currentHoursElement = document.querySelector(
+        `#current-hrs-${categoryTitleId}`
+      );
+      currentHoursElement.textContent = current;
+
+      const previousHoursElement = document.querySelector(
+        `#previous-hrs-${categoryTitleId}`
+      );
+
+      previousHoursElement.textContent =
+        previous === 1
+          ? `Last Week - ${previous}hr`
+          : `Last Week - ${previous}hrs`;
     }
   });
 }
 
+function updateAriaSelected(event) {
+  event.preventDefault();
+  cardBtns.forEach(button => {
+    button.ariaSelected = false;
+    event.currentTarget.ariaSelected = true;
+  })
+  
+  
+}
+
+function changeFocus(event) {
+  event.preventDefault()
+  cards[0].focus()
+}
 
 
+// cards.forEach((card, index) => {
+//   card.addEventListener('keydown', (event) => {
+    
+//     if (event.key === "Tab") {
+//       console.log(event.key);
+//       event.preventDefault();
+//       (cards[index + 1] || cardBtns[0]).focus()
+//     }
+//   })
+// })
